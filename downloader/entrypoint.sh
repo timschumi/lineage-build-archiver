3064835941ac830b992a22045d56175f598d85a1
Tim Schumacher <timschumi@gmx.de>
2022-12-16 16:19:14 +0100

    Index builds into a database

--- downloader/entrypoint.sh
+++ downloader/entrypoint.sh
@@ -2,5 +2,12 @@
 
 while true; do
     ./download.py --output /builds "${@}"
+
+    if [ -n "${POSTGRES_HOST}" ]; then
+        # TODO: Clear non-existent local files from the database automatically.
+        # TODO: Do this directly while downloading.
+        ./index.py /builds
+    fi
+
     sleep `expr 60 \* ${UPDATE_INTERVAL}`
 done
