72e6db4c0380eb32485dd8cd73d090d4a9932996
Tim Schumacher <timschumi@gmx.de>
2024-02-24 04:35:02 +0100

    downloader: Make it a one-shot Docker container

--- downloader/entrypoint.sh
+++ downloader/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash -e
 
-while true; do
-    ./download.py ${DOWNLOAD_ARGS}
-    ./prune.py ${PRUNE_ARGS}
-
-    sleep `expr 60 \* ${UPDATE_INTERVAL}`
-done
+./download.py ${DOWNLOAD_ARGS}
+./prune.py ${PRUNE_ARGS}
