97b18dcda384e8ceec52dc7a8e3c312f7376a6d2
Tim Schumacher <timschumi@gmx.de>
2024-04-09 18:07:07 +0200

    portal: Allow filtering for devices

--- portal/static/overview.js
+++ portal/static/overview.js
@@ -1,5 +1,9 @@
 const device_table_map = new Map();
 
+const filter_refresh_delay = 250;
+const filter_input = document.getElementById('filter');
+filter_input.value = window.location.hash.substring(1);
+
 fetch('/api/builds', { cache: "reload" })
     .then((response) => response.json())
     .then((builds) => {
@@ -42,7 +46,15 @@ fetch('/api/builds', { cache: "reload" })
 
         const device_list = document.getElementById('device_list');
         device_table_map_sorted.forEach((value) => {
+            value.style.display = "none";
             device_list.appendChild(value);
+        });
+
+        let filter_timeout_id = setTimeout(refresh_filter, 0);
+        filter_input.addEventListener("input", (event) => {
+            history.replaceState(undefined, undefined, "#" + filter_input.value);
+            clearTimeout(filter_timeout_id);
+            filter_timeout_id = setTimeout(refresh_filter, filter_refresh_delay);
         });
     })
 
@@ -62,3 +74,14 @@ fetch('/api/statistics', { cache: "reload" })
         fill_statistic("stat_build_size_average", human_readable_size(statistics["build_size_average"]))
         fill_statistic("stat_build_size_total", human_readable_size(statistics["build_size_average"] * statistics["device_version_count"]))
     })
+
+function refresh_filter() {
+    const filter_value = filter_input.value.toLowerCase();
+    device_table_map.forEach((value, key) => {
+        const compare_value = key.toLowerCase();
+        if (compare_value.includes(filter_value))
+            value.style.display = "";
+        else
+            value.style.display = "none";
+    });
+}
